93afdcd926bd904949dc75d656cb7bdcc82c43f8
Brent Dombrowski <brent@workstation-mini>
2011-10-15 18:47:55 -0700

    Removed hard codings for equations. Added normalizer.

--- BD-ListView/src/com/listview/HelloListViewActivity.java
+++ BD-ListView/src/com/listview/HelloListViewActivity.java
@@ -13,8 +13,8 @@
 package com.listview;
 
 import java.util.Hashtable;
+import java.util.ArrayList;
 
-import android.app.Activity;
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -30,16 +30,49 @@ public class HelloListViewActivity extends ListActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        String[] equations = getResources().getStringArray(R.array.equations);
-        String[] scores = getResources().getStringArray(R.array.scores);
-    
         
-        for (int i = 0; i < equations.length ; ++i) {
-        	equationTable.put(equations[i], Integer.parseInt(scores[i]));
+        // Create the lists of valid equations and their scores
+        ArrayList equationsAL = new ArrayList(152);
+        ArrayList scoresAL = new ArrayList(152);
+        
+        for (int i = 0; i < 10; ++i) {
+        	for (int j = 0; j < 10; ++j) {
+        		if ((i + j < 10) && (i <= j)) {
+        			equationsAL.add(i + "+" + j + "=" + (i + j) );
+        			scoresAL.add((i+j));
+        		}
+        		if (i - j >= 0) {
+        			equationsAL.add(i + "-" + j + "=" + (i - j));
+        			scoresAL.add((i+j+10));
+        		}
+        		if ((i * j < 10) && (i < j)) {
+        			equationsAL.add(i + "*" + j + "=" + (i * j));
+        			scoresAL.add((i+j+20));
+        		}
+        	}
+        }
+    
+        // Load the hash table
+        for (int i = 0; i < scoresAL.size() ; ++i) {
+        	equationTable.put(equationsAL.get(i), scoresAL.get(i));
         }
         
-        setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, equations));
+        // Add in a bogus equation for testing.
+        equationsAL.add("1+1=1");
+
+        // Add in the non-normalized equations
+        for (int i = 0; i < 10; ++i) {
+        	for (int j = 0; j < 10; ++j) {
+        		if ((i + j < 10) && (i > j)) {
+        			equationsAL.add(i + "+" + j + "=" + (i + j));
+        		}
+        		if ((i * j < 10) && (i > j)) {
+        			equationsAL.add(i + "*" + j + "=" + (i * j));
+        		}
+        	}
+        }
+        
+        setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, equationsAL));
         
         ListView lv = getListView();
         lv.setTextFilterEnabled(true);
@@ -49,18 +82,51 @@ public class HelloListViewActivity extends ListActivity {
         		// When clicked, show a toast with the TextView text
         		int score = CheckEquation(((TextView) view).getText());
         		Toast.makeText(getApplicationContext(), Integer.toString(score), Toast.LENGTH_SHORT).show();
-        		//Toast.makeText(getApplicationContext(), "Click", Toast.LENGTH_SHORT).show();
         		}
         });
     }
     
+    /*
+     * Look up an equation in the hash table.
+     * 
+     * INPUT: string representing the equation to look up.
+     * OUTPUT: The score or -1 if the equation is not found. 
+     */
     public int CheckEquation(CharSequence inputString) {
-    	Integer result = (Integer)equationTable.get(inputString);
+    	Integer result = (Integer)equationTable.get(NormalizeEquation(inputString));
     	if (result != null) {
+    		// Note, it would be possible to pull equation out of the table here.
     		return result;
     	}
-    	return 0;
+    	return -1;
     	
+    }
+    
+    public String NormalizeEquation(CharSequence inputCharSequence) {
+    	// Make sure it's the right length
+    	if (inputCharSequence.length() != 5) {
+    		return null;
+    	}
+    	// CharSequences are read only, convert to a string to manipulate.
+    	String inputString = inputCharSequence.toString();
+    	
+    	// Is there an = at position 2?
+    	if (inputString.charAt(1) == '=') {
+    		// Need to flip it
+    		inputString = inputString.substring(2) + "=" + inputString.charAt(0);
+    	}
+    	// Is it addition or multiplication?
+    	if ( (inputString.charAt(1) == '+') || (inputString.charAt(1) == '*')) {
+    		// Make sure the smaller number is on the left.
+    		char x = inputString.charAt(0);
+    		char y = inputString.charAt(2);
+    		if (x > y) { // Hope this part works.
+    			inputString = Character.toString(y) + inputString.charAt(1) + Character.toString(x) + inputString.substring(3, 5);
+    			//Toast.makeText(getApplicationContext(), "N:" + inputString, Toast.LENGTH_SHORT).show();
+    		}
+    	}
+    	
+    	return inputString;
     }
     
     private Hashtable equationTable = new Hashtable();
