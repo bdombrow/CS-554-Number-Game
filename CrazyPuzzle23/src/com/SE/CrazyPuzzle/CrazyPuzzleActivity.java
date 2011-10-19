69d90540de7200ace616bf05cd6d401f296d3b6e
Brent Dombrowski <bdombrow@cecs.pdx.edu>
2011-10-19 15:24:28 -0700

    Added CrazyPuzzle23. Added Toast for coordinate for debugging. Issue with coordinates with 2.3.

---
+++ CrazyPuzzle23/src/com/SE/CrazyPuzzle/CrazyPuzzleActivity.java
@@ -0,0 +1,67 @@
+package com.SE.CrazyPuzzle;
+
+import com.SE.CrazyPuzzle.R;
+import com.SE.CrazyPuzzle.PuzzleView;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
+
+public class CrazyPuzzleActivity extends Activity implements View.OnTouchListener
+{
+    private PuzzleView mPuzzleView;
+    
+    private static String ICICLE_KEY = "puzzle-view";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) 
+    {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.main);
+        
+        mPuzzleView = (PuzzleView) findViewById(R.id.puzzle);
+        mPuzzleView.setOnTouchListener(this);
+
+        if (savedInstanceState == null) 
+        {
+            // We were just launched -- set up a new game
+        	//mPuzzleView.setMode(PuzzleView.READY);
+        } 
+        else 
+        {
+            // We are being restored
+            Bundle map = savedInstanceState.getBundle(ICICLE_KEY);
+            if (map != null) 
+            {
+            	mPuzzleView.restoreState(map);
+            } 
+            else 
+            {
+            	//mPuzzleView.setMode(PuzzleView.PAUSE);
+            }
+        }
+    }
+
+    @Override
+    protected void onPause() 
+    {
+        super.onPause();
+        // Pause the game along with the activity
+        //mPuzzleView.setMode(PuzzleView.PAUSE);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) 
+    {
+        //Store the game state
+        outState.putBundle(ICICLE_KEY, mPuzzleView.saveState());
+    }
+
+	public boolean onTouch(View v, MotionEvent event) 
+	{
+		return mPuzzleView.onTouch(v, event);
+	}
+}
